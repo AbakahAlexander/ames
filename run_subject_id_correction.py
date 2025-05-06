8e647a270cba716d6ec6635d73fae73309200889
Alexander Abakah <alexanderabakah70@gmail.com>
2025-05-06 11:38:32 -0700

    added token as environment variable

--- run_subject_id_correction.py
+++ run_subject_id_correction.py
@@ -1,13 +1,13 @@
 from caltechdata_api import get_metadata
 from ames.matchers import edit_subject
-
+import os
 
 def all_corrected():
 
   record = "2d2wf-j0256"
   subjects_to_correct = {'Biological sciences': 'http://www.oecd.org/science/inno/38235147.pdf?1.6', 'Chemical sciences': 'http://www.oecd.org/science/inno/38235147.pdf?1.4', 'Computer and information sciences': 'http://www.oecd.org/science/inno/38235147.pdf?1.2'}
 
-  metadata = edit_subject("2d2wf-j0256", "FVyjwsxBvfNXm5NmmfL8fKGI8hhA6puT9pNJO8PAyrLlNYdeMjfjhBVvuhbs" , subjects_to_correct)
+  metadata = edit_subject("2d2wf-j0256", os.environ.get("CALTECH_DATA_API"), subjects_to_correct)
 
 
   for i in metadata["subjects"]:
